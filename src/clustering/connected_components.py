772f76100025bd1dcf7659010b02f12c48a5f7b3
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2022-06-22 14:56:29 +0300

    fixed issue with tqdm

--- src/clustering/connected_components.py
+++ src/clustering/connected_components.py
@@ -17,10 +17,11 @@ class ConnectedComponentsClustering:
         self._progress_bar: tqdm
 
     def process(self, graph: nx.Graph) -> pd.DataFrame:
-
-
+        '''
+        TODO: comment
+        '''
         connected_components = nx.connected_components(graph)
-        self._progress_bar = tqdm(total=len(connected_components), desc=self._method_name)
+        self._progress_bar = tqdm(total=len(list(connected_components)), desc=self._method_name)
 
         pairs_df = pd.DataFrame(columns=["id1", "id2"])
         num_of_pairs = 1
@@ -33,4 +34,5 @@ class ConnectedComponentsClustering:
                     pairs_df.loc[num_of_pairs] = [sorted_component[id1_index], sorted_component[id2_index]]
                     num_of_pairs += 1
             self._progress_bar.update(1)
+        
         return pairs_df
