b88b5c296e0ac2d61f233701c17642b555dcb6c4
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2022-06-27 18:54:06 +0300

    updated pairs

--- src/evaluation/scores.py
+++ src/evaluation/scores.py
@@ -20,8 +20,8 @@ from src.blocks.utils import drop_single_entity_blocks
 
 class Evaluation:
 
-    def __init__(self) -> None:
-        self.F1: float
+    def __init__(self, data: Data) -> None:
+        self.f1: float
         self.recall: float
         self.precision: float
         self.accuracy: float
@@ -30,27 +30,53 @@ class Evaluation:
         self.true_negatives = 0
         self.false_positives = 0
         self.false_negatives = 0
+        self.total_matching_pairs = 0
+        self._entity_cluster_index = dict()
+        self.data: Data = data
 
-    def report(self, predicted: list, data: Data) -> None:
+    def report(self, predicted_clusters: list, data: Data) -> None:
 
         gt = data.ground_truth
 
-        self._inverse_groups(predicted)
+        self._inverse_clusters(predicted_clusters)
 
         for pair in gt.itterrows():
             id1 = pair['id1']
             id2 = pair['id2']
 
-            if (id2 in self._inverted_index and id1 in self._inverted_index[id2]) or \
-                (id1 in self._inverted_index and id2 in self._inverted_index[id1]):
+            if self._entity_cluster_index[id1] == self._entity_cluster_index[id2]:
                 self.true_positives += 1
-                
-    def _inverse_groups(self, groups: list) -> dict:
+            else:
+                self.false_negatives += 1
 
-        self._inverted_index = dict()
-        for group, group_id in (groups, range(0, len(groups))):
-            for id in group:
-                self._inverted_index.setdefault(id, set())
-                self._inverted_index[id].add(group_id)
+        self.false_positives = self.total_matching_pairs - self.true_positives
+        self.true_negatives = self.total_matching_pairs - self.false_positives
+        
 
-        return self._inverted_index
+        # self.accuracy = 
+        # self.precision = 
+        # self.recall = 
+        # self.f1 = 
+        self.print_results()
+
+    def print_results(self) -> None:
+        print()
+
+    def _inverse_clusters(self, clusters: list) -> dict:
+
+        for cluster, cluster_id in (clusters, range(0, len(clusters))):
+            cluster_entities_d1 = 0
+            cluster_entities_d2 = 0
+            for id in cluster:
+                self._entity_cluster_index[id] = cluster_id
+
+                if not self.data.is_dirty_er:
+                    if id < self.data.dataset_limit:
+                        cluster_entities_d1 += 1
+                    else:
+                        cluster_entities_d2 += 1
+
+            if self.data.is_dirty_er:
+                self.total_matching_pairs += len(cluster)*(len(cluster)-1)/2
+            else:
+                self.total_matching_pairs += cluster_entities_d1*cluster_entities_d2
