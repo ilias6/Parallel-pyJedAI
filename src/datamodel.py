4d4eb7e35dd41ceb7dc3a1b244172b36386c50fa
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2022-07-04 17:10:32 +0300

    fixed bugs in datamodel

--- src/datamodel.py
+++ src/datamodel.py
@@ -30,22 +30,16 @@ class Data:
     def process(self, text_cleaning_method=None) -> None:
         
         if self.attributes: self.dataset_1 = self.dataset_1[self.attributes]
-        self.entities = self.dataset_1[self.attributes] = self.dataset_1[self.attributes].apply(text_cleaning_method)
-        self.entities_d1 = self.dataset_1.apply(" ".join, axis=1)        
+        self.entities = self.dataset_1 = self.dataset_1.apply(text_cleaning_method)
+        self.entities_d1 = self.dataset_1.apply(" ".join, axis=1)
         
         if not self.is_dirty_er:
             if self.attributes: self.dataset_2 = self.dataset_2[self.attributes]
-            self.dataset_2[self.attributes] = self.dataset_2[self.attributes].apply(text_cleaning_method)
-
-            if self.attributes:
-                self.entities_d2 = self.dataset_2[self.attributes].apply(" ".join, axis=1)
-            else:
-                self.entities_d2 = self.dataset_2.apply(" ".join, axis=1)
-
+            self.dataset_2 = self.dataset_2.apply(text_cleaning_method)
+            self.entities_d2 = self.dataset_2.apply(" ".join, axis=1)
             self.entities = pd.concat([self.dataset_1, self.dataset_2])
             self.ground_truth.iloc[:, 1] = self.ground_truth.iloc[:, 1] + len(self.ground_truth.iloc[:, 0])
 
-        
     def print_specs(self):
         print("Type of Entity Resolution: ", "Dirty" if self.is_dirty_er else "Clean-Clean" )
         print("Number of entities in D1: ", self.num_of_entities_1)
