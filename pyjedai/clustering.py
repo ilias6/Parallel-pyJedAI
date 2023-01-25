1134ddc0f303016b40f486316652d8cf2081d2e8
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2023-01-25 19:43:11 +0200

    finished abstraction in Clustering

--- pyjedai/clustering.py
+++ pyjedai/clustering.py
@@ -1,9 +1,59 @@
 from queue import PriorityQueue
-from networkx import connected_components, Graph
 from time import time
+
+from networkx import Graph, connected_components
 from tqdm.autonotebook import tqdm
 
-class ConnectedComponentsClustering:
+from .datamodel import Data, PYJEDAIFeature
+from .evaluation import Evaluation
+from .utils import are_matching
+
+
+class AbstractClustering(PYJEDAIFeature):
+    
+    def __init__(self) -> None:
+        super().__init__()
+
+    def evaluate(self,
+                prediction,
+                export_to_df: bool = False,
+                export_to_dict: bool = False,
+                with_classification_report: bool = False,
+                verbose: bool = True) -> any:
+
+        if prediction is None:
+            if self.blocks is None:
+                raise AttributeError("Can not proceed to evaluation without build_blocks.")
+            else:
+                eval_blocks = self.blocks
+        else:
+            eval_blocks = prediction
+            
+        if self.data is None:
+            raise AttributeError("Can not proceed to evaluation without data object.")
+
+        if self.data.ground_truth is None:
+            raise AttributeError("Can not proceed to evaluation without a ground-truth file. " + 
+                    "Data object has not been initialized with the ground-truth file")
+
+        eval_obj = Evaluation(self.data)
+        true_positives = 0
+        entity_index = eval_obj._create_entity_index_from_clusters(eval_blocks)
+        for _, (id1, id2) in self.data.ground_truth.iterrows():
+            id1 = self.data._ids_mapping_1[id1]
+            id2 = self.data._ids_mapping_1[id2] if self.data.is_dirty_er else self.data._ids_mapping_2[id2]
+            if id1 in entity_index and    \
+                id2 in entity_index and are_matching(entity_index, id1, id2):
+                true_positives += 1
+
+        eval_obj.calculate_scores(true_positives=true_positives)
+        eval_obj.report(self.method_configuration(),
+                        export_to_df,
+                        export_to_dict,
+                        with_classification_report,
+                        verbose)
+
+class ConnectedComponentsClustering(AbstractClustering):
     """Creates the connected components of the graph. \
         Applied to graph created from entity matching. \
         Input graph consists of the entity ids (nodes) and the similarity scores (edges).
@@ -15,9 +65,9 @@ class ConnectedComponentsClustering:
                     "transitive closure of the similarity graph."
 
     def __init__(self) -> None:
-        self.execution_time: float
+        super().__init__()
 
-    def process(self, graph: Graph) -> list:
+    def process(self, graph: Graph, data: Data) -> list:
         """NetworkX Connected Components Algorithm in the produced graph.
 
         Args:
@@ -27,33 +77,16 @@ class ConnectedComponentsClustering:
             list: list of clusters
         """
         start_time = time()
+        self.data = data
         clusters = list(connected_components(graph))
         self.execution_time = time() - start_time
         return clusters
 
-    def method_configuration(self) -> dict:
-        """Returns configuration details
-        """
-        return {
-            "name" : self._method_name,
-            "parameters" : self._configuration(),
-            "runtime": self.execution_time
-        }
-
     def _configuration(self) -> dict:
         return {}
 
-    def report(self) -> None:
-        """Prints Block Building method configuration
-        """
-        print(
-            "Method name: " + self._method_name +
-            "\nMethod info: " + self._method_info +
-            "\nRuntime: {:2.4f} seconds".format(self.execution_time)
-        )
 
-
-class UniqueMappingClustering:
+class UniqueMappingClustering(AbstractClustering):
     """Prunes all edges with a weight lower than t, sorts the remaining ones in
         decreasing weight/similarity and iteratively forms a partition for
         the top-weighted pair as long as none of its entities has already
@@ -74,11 +107,11 @@ class UniqueMappingClustering:
             similarity_threshold (float, optional): Prunes all edges with a weight
                 lower than this. Defaults to 0.1.
         """
+        super().__init__()
         self.similarity_threshold: float = similarity_threshold
-        self.execution_time: float
         print("Unique Mapping Clustering can only be performed in Clean-Clean Entity Resolution.")
 
-    def process(self, graph: Graph) -> list:
+    def process(self, graph: Graph, data: Data) -> list:
         """NetworkX Connected Components Algorithm in the produced graph.
 
         Args:
@@ -89,6 +122,7 @@ class UniqueMappingClustering:
         """
         start_time = time()
         matched_entities = set()
+        self.data = data
         new_graph = Graph()
         priority_queue = PriorityQueue(maxsize=graph.number_of_edges()*2)
         for x in graph.edges(data=True):
@@ -103,27 +137,9 @@ class UniqueMappingClustering:
             matched_entities.add(entity_1)
             matched_entities.add(entity_2)
 
-        clusters = ConnectedComponentsClustering().process(new_graph)
+        clusters = ConnectedComponentsClustering().process(new_graph, data)
         self.execution_time = time() - start_time
         return clusters
 
-    def method_configuration(self) -> dict:
-        """Returns configuration details
-        """
-        return {
-            "name" : self._method_name,
-            "parameters" : self._configuration(),
-            "runtime": self.execution_time
-        }
-
     def _configuration(self) -> dict:
         return {}
-
-    def report(self) -> None:
-        """Prints Block Building method configuration
-        """
-        print(
-            "Method name: " + self._method_name +
-            "\nMethod info: " + self._method_info +
-            "\nRuntime: {:2.4f} seconds".format(self.execution_time)
-        )
